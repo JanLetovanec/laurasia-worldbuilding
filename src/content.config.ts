bfd52b87e1cc55d4fbab52978c3a96038e338635
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-06-29 12:35:49 +0100

    Add a content collection for all organisations

---
+++ src/content.config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const organisations = defineCollection({
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "./src/content/organisation",
+  }),
+  schema: z.object({
+    title: z.string(),
+    slug: z.string(),
+  }),
+});
+
+export const collections = { organisations };
