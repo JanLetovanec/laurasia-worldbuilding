12864c60e905f9e3fec29ffe3f0fe6d078832d24
CJMinecraft <cjminecraft.theclever.me@gmail.com>
2025-06-29 20:42:59 +0100

    Add tables of contents processing when there is a contents header

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
+  markdown: {
+    remarkPlugins: [[remarkToc, {heading: 'contents', maxDepth: 3}]]
+  }
 });
