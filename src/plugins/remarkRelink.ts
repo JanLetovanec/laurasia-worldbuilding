2cd1010b8d3642559d52607bd0640c9816f53a80
Jan Let <janko.let@gmail.com>
2025-07-06 03:51:18 +0100

    bail early if irrelevant

--- src/plugins/remarkRelink.ts
+++ src/plugins/remarkRelink.ts
@@ -1,11 +1,28 @@
 import { visit, type Visitor} from "unist-util-visit";
-import type { Root, Node } from "mdast";
+import type { Root, Node , Text, Parent} from "mdast";
+
+const isText = (node: Node) : node is Text => {
+  return node.type == 'text';
+};
+
+const isParent = (node: Node | undefined) : node is Parent => {
+  if (node === undefined) {
+    return false;
+  }
+
+  const maybeParent = node as unknown as Parent;
+  return maybeParent.children !== undefined;
+}
+
 
 const visitAndRelink : Visitor = (
   visitee: Node,
   index: number | undefined,
   parent: Node | undefined) => {
-  // 1. If you are text node bail
+  if (!isText(visitee) || !isParent(parent) || index == null) {
+    return;
+  }
+
   // 2. If you do not contain `[[foo]]`, bail
   // 3. Otherwise "map" the node to "previous stuff" [foo](foo) "next stuff"
 }
