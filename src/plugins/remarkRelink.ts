6bf0449ae16527ecb64a595fbd512f7574e07620
Jan Let <janko.let@gmail.com>
2025-07-06 03:38:36 +0100

    Add boiler plugin

---
+++ src/plugins/remarkRelink.ts
@@ -0,0 +1,11 @@
+import type { Root } from "mdast";
+
+export default function remarkRelink() {
+  /**
+   * @param {Root} tree
+   * @return {undefined}
+   */
+  return (tree: Root): undefined => {
+    //TODO: transform the `tree`
+  };
+}
